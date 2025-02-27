68ace5a00c6bb36341e86bd55eb5a9161704b75e
Zarpany <juanes7gallego@gmail.com>
2025-02-27 09:21:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("JavaScript cargado correctamente");
 
 window.onload = function() {
     // Ejemplo: Agregar una alerta al cargar la página
-    alert("¡Bienvenido a la Página de Fans de Laufey!");
+    alert("!!!Welcome to the Fan page of Laufey!!!");
 
     // Ejemplo: Cambiar el color de fondo de los divs con la clase 'song-info' al hacer clic
     const songInfoDivs = document.querySelectorAll('.song-info');
